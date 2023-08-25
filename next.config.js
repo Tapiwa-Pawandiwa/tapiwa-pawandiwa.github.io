ea62036f36400ebe9b889be1edb19a1785b5c50a
Tapiwa-Pawandiwa <tapiwataps13@gmail.com>
2023-08-25 23:47:55 +0200

    removed fluff

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath:"/tapiwa-pawandiwa.github.io",
+};
 
-}
-
-module.exports = nextConfig
+module.exports = nextConfig;
