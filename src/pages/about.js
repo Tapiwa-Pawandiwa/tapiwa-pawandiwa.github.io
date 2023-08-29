748402df5573a5b5ae9bea3043f5211a7864a3eb
Tapiwa-Pawandiwa <tapiwataps13@gmail.com>
2023-08-29 13:28:15 +0200

    add content-fixfavicon

--- src/pages/about.js
+++ src/pages/about.js
@@ -18,29 +18,49 @@ const about = () => {
       <TransitionEffect />
       <main className="flex w-full flex-col item-center justify-center">
         <Layout className="pt-20">
-          <AnimatedText text="Bits and Bytes" className="mb-16 lg:!text-7xl sm:!text-6xl xs:!text-4xl sm:mb-8" type="header" />
-          <div className="grid w-full grid-cols-8 gap-16 sm:gap-8" >
+          <AnimatedText
+            text="Bits and Bytes"
+            className="mb-16 lg:!text-7xl sm:!text-6xl xs:!text-4xl sm:mb-8"
+            type="header"
+          />
+          <div className="grid w-full grid-cols-8 gap-16 sm:gap-8">
             <div className="col-span-4 flex flex-col items-start justify-start xl:col-span-4 md:order-2 md:col-span-8">
               <h2 className="mb-4 text-3xl font-bold uppercase text-dark/75 xl:items-center ">
                 About Me
               </h2>
               <p className="font-poppinsLight text-xl">
-                Hi there, I enjoy creating applications, I am currently a
-                Masters student in Computer Science. I am a bit of a geek, I
-                enjoy creating tantilizing user interfaces and innovating ideas.
-                I consider myself a Developer and a Designer. I have been coding
-                in various languages for the past 6 years, from Web applications
-                to Mobile Apps.
+                Hi there,My name is Tapiwa and I enjoy creating software
+                applications. I am currently a Masters student in Computer
+                Science. I enjoy building creating innovative projects. I love
+                Art and Design and as a result of this I also enjoy creating
+                tantilizing user interfaces and user experiences. I consider
+                myself a Developer and a Designer. I have been coding in various
+                languages for the past 6 years, from Web applications to Mobile
+                Apps.
               </p>
               <p className="my-4 font-poppinsLight text-xl">
-                I believe in the power of technology to transform lives and I am
-                passionate about using my skills to make a difference. I would
-                take part in various University Competitions which encouraged
-                innovation and problem solving.
+                I firmly believe in the transformative potential of technology
+                to impact lives, and I am deeply enthusiastic about utilizing my
+                skills to bring about positive change. My journey has involved
+                active participation in various university competitions, such as
+                the Hultz Prize Competition and the Innovation Think Tank at
+                Rhodes University. <br/> One notable experience was our participation
+                in a blockchain competition, where my friends and I were
+                challenged to devise an inventive blockchain solution. Our
+                efforts led us to secure the first-place position in the
+                competition. Alongside this, I played an integral role in the
+                Sudo-Soc university society. Within Sudo-Soc, our focus was on
+                educating fellow students in coding, orchestrating coding
+                community events, and coordinating hackathons.  <br/>Furthermore, I
+                was part of an initiative within Sudo-Soc that entailed
+                establishing a computer lab within the local community. This
+                endeavor exemplified our commitment to extending the benefits of
+                technology beyond the university setting, bridging the digital
+                divide, and fostering technological inclusivity
               </p>
               <p className="my-4 font-poppinsLight text-xl">
                 I believe technology is forever changing, as a result of this I
-                am not afraid to learn new languages, skills and technologies.
+                am not afraid to learn new languages, skills and technologies. I am currently researching and experimenting with Blockchain and Web3. 
               </p>
               <p className="my-4 font-poppinsLight text-xl">
                 I have worked on React Websites. React Native Mobile
@@ -55,8 +75,8 @@ const about = () => {
                 </span>
                 Technology empowers people to do what they want to do. It lets
                 people be creative. It lets people be productive. It lets people
-                learn things they didn&apos;t think they could learn before, and so
-                in a sense, it is all about potential.
+                learn things they didn&apos;t think they could learn before, and
+                so in a sense, it is all about potential.
                 <span className="absolute bottom-0 right-0 transform translate-x-2/3 translate-y-1/4 text-5xl text-primary">
                   &rdquo;
                 </span>
@@ -65,11 +85,12 @@ const about = () => {
               <h3 className="mb-4 italic mt-10 text-2xl uppercase font-bold  text-dark/75">
                 Hobbies
               </h3>
-              <div className="md:order-3">
+              <div className="md:order-3 justify-center">
                 <ul className="list-disc list-inside">
                   <li className="font-poppinsLight text-xl">Painting </li>
                   <li className="font-poppinsLight text-xl">Playing Guitar</li>
                   <li className="font-poppinsLight text-xl">Travelling </li>
+                  <li className="font-poppinsLight text-xl">Music  </li>
                 </ul>
               </div>
             </div>
